71af429b67e42c9c6b8f4507d2d34118506587d9
Andrei Prado <andrei.s.do.prado@accenture.com>
2025-09-18 01:46:59 -0300

    Adjust menu behaviour

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import "./navbar.component.scss";
-import logo from "../../assets/logo-black-white.png";
+import logo from "../../assets/logo-blue.png";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 10) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar${open ? ' open' : ''}${isScrolled ? ' scrolled' : ''}`}>
       {/* Logo */}
       <div className="logo">
         <a href="/home"><img src={logo} alt="Z'ele Church" /></a>
@@ -15,18 +26,19 @@ export default function Navbar() {
       {/* Botão hamburguer → vira X */}
       <div className="menu">
         <button
-          className={`hamburger ${open ? "open" : ""}`}
+          className={`hamburger ${open ? 'open' : ''}`}
           onClick={() => setOpen(!open)}
         >
+          <span></span>
           <span></span>
           <span></span>
         </button>
       </div>
 
       {/* Menu lateral */}
-      <div className={`side-menu ${open ? "show" : ""}`}>
+      <div className={`side-menu ${open ? 'show' : ''}`}>
         <ul>
-          <li><a href="signup" onClick={() => setOpen(false)}>Quero ser membro</a></li>
+          <li><a href="/signup" onClick={() => setOpen(false)}>Quero ser membro</a></li>
         </ul>
       </div>
     </nav>
